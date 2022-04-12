82715d1ab76ed8ffad3b9f4804414e495576af23
Pranav Karawale <pranav@karawale.in>
2022-04-12 14:05:46 +0530

    License headers bad :(

--- src/plugins/link.ts
+++ src/plugins/link.ts
@@ -1,19 +1,3 @@
-/**
- * Copyright 2022 Pranav Karawale
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import { syntaxTree } from '@codemirror/language';
 import {
     Decoration,
@@ -21,7 +5,7 @@ import {
     EditorView,
     ViewPlugin,
     ViewUpdate,
-    WidgetType,
+    WidgetType
 } from '@codemirror/view';
 import { headingSlugField } from '../state/heading-slug';
 
@@ -40,14 +24,12 @@ export class GoToLinkWidget extends WidgetType {
             // Handle links within the markdown document.
             const slugs = view.state.field(headingSlugField);
             anchor.addEventListener('click', () => {
-                const pos = slugs.find(
-                    (h) => h.slug === this.link.slice(1)
-                )?.pos;
+                const pos = slugs.find(h => h.slug === this.link.slice(1))?.pos;
                 // pos could be zero, so instead check if its undefined
                 if (typeof pos !== 'undefined') {
                     const tr = view.state.update({
                         selection: { anchor: pos },
-                        scrollIntoView: true,
+                        scrollIntoView: true
                     });
                     view.dispatch(tr);
                 }
@@ -71,10 +53,10 @@ function getLinkAnchor(view: EditorView) {
                 if (type.name !== 'URL') return;
                 const dec = Decoration.widget({
                     widget: new GoToLinkWidget(view.state.sliceDoc(from, to)),
-                    side: 1,
+                    side: 1
                 });
                 widgets.push(dec.range(to, to));
-            },
+            }
         });
     }
 
@@ -96,12 +78,12 @@ export const goToLinkPlugin = ViewPlugin.fromClass(
                 this.decorations = getLinkAnchor(update.view);
         }
     },
-    { decorations: (v) => v.decorations }
+    { decorations: v => v.decorations }
 );
 
 const baseTheme = EditorView.baseTheme({
     '.cm-link': {
         cursor: 'pointer',
-        textDecoration: 'underline',
-    },
+        textDecoration: 'underline'
+    }
 });
