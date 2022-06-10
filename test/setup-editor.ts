01f0ce28c92316f3fc3976ef072047a7d73a7c73
Pranav Karawale <pranav@karawale.in>
2022-06-10 18:25:22 +0000

    Remove unnecessary plugins from test bed

--- test/setup-editor.ts
+++ test/setup-editor.ts
@@ -1,10 +1,6 @@
 import { minimalSetup, EditorView } from 'codemirror';
 import { EditorState } from '@codemirror/state';
 import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
-import { indentWithTab } from '@codemirror/commands';
-import { keymap } from '@codemirror/view';
-import { yaml } from '@codemirror/legacy-modes/mode/yaml';
-import { StreamLanguage } from '@codemirror/language';
 import ixora, { frontmatter } from '../dist';
 
 /**
@@ -16,12 +12,10 @@ export function setup(el: HTMLElement) {
     const editor = new EditorView({
         state: EditorState.create({
             extensions: [
-                keymap.of([indentWithTab]),
                 markdown({
                     base: markdownLanguage,
                     extensions: [frontmatter]
                 }),
-                StreamLanguage.define(yaml),
                 EditorView.lineWrapping,
 
                 minimalSetup,
