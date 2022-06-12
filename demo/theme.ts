339e1125a90d8fd972bbc6219ffb5113f76bbe63
Pranav Karawale <pranav@karawale.in>
2022-06-12 10:33:06 +0000

    Change demo styles a bit

--- demo/theme.ts
+++ demo/theme.ts
@@ -5,12 +5,15 @@ import { EditorView } from 'codemirror';
 
 export const theme = EditorView.theme(
     {
-        '&': { height: '100%', fontSize: '20px' },
+        '&': { height: '100%' },
         '.cm-scroller': {
             width: '80ch',
             margin: 'auto',
             overflow: 'auto',
             fontFamily: 'sans-serif'
+        },
+        '.cm-content': {
+            fontSize: '18px'
         }
     },
     { dark: false }
