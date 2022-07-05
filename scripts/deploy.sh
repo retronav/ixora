83500193c5208c01d9ec4b540d7893f7cfe219f3
Pranav Karawale <pranav@karawale.in>
2022-07-05 17:40:55 +0530

    Use Codeberg Pages

---
+++ scripts/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -ex
+
+export DEPLOYMENT_BRANCH="pages"
+export DEPLOYMENT_DIR="dist"
+
+if [[ ! -z $CI ]]; then
+    mkdir -p ~/.ssh
+    echo $GIT_SSH_KEY >> ~/.ssh/id_rsa
+    git config "user.name" "codeberg-ci"
+    git config "user.email" "noreply@noreply.codeberg.org"
+fi
+
+cd www/
+pnpm build
+
+mkdir /tmp/pages.git
+rsync -av $DEPLOYMENT_DIR/* /tmp/pages.git/
+
+cd /tmp/pages.git/
+git init
+git remote add origin git@codeberg.org:retronav/ixora.git
+git checkout --orphan $DEPLOYMENT_BRANCH
+git add .
+git commit -m "Deploy site"
+git push -f origin pages
+
